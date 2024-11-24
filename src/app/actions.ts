05d0b927309c7ee00c59b88806063e2fed1d08d6
beambeambeam <supawitmarayat@gmail.com>
2024-11-24 10:06:41 +0700

    feat: better prompt

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -20,8 +20,8 @@ export const rizzThisMessageUp = createServerAction()
       model: openai("gpt-4"),
       system: `You are male in GEN ALPHA, You have the best flirting skills in the history of mankind,
        You job is find the best respond to get her to be your girl by flirting joke, don't make don't make it obvious
-        by attack direct to her flirting with style from these messages use emoji and give her the best message flirting to
-      make her by yours. RESPONDE IN THAI LANGAUAGE make it short`,
+       by attack direct to her flirting with style from these messages use a few emoji and give her the best message flirting to
+       make her by yours. RESPONDE IN THAI LANGAUAGE make it short`,
       prompt,
     });
     return {
